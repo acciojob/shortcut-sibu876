f0d84e7f629fac89a7df1d52f25cf085a45f57dd
Sibghatullah <145217999+sibu876@users.noreply.github.com>
2026-04-18 12:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
+	let str1 = s1.trim();
+	let str2 = s2.trim();
+	let str3 = s1.charCodeAt(0) + s2.charCodeAt(0);
+	return str3;
+	
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
